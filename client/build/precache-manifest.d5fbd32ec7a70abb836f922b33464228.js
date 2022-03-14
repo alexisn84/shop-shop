e2534ba49755a68d21ae571a8c9609371ad3f7c4
alexisn84 <elamrani.alexis@gmail.com>
2022-03-14 16:00:24 -0400

    components productList and CategoryMenu

---
+++ client/build/precache-manifest.d5fbd32ec7a70abb836f922b33464228.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c722d9d9b2a3150d4beee5a6748557f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "803315e9490fe1c795bb",
+    "url": "/static/css/main.db415adc.chunk.css"
+  },
+  {
+    "revision": "690305e09c74568004e3",
+    "url": "/static/js/2.be90de22.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.be90de22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "803315e9490fe1c795bb",
+    "url": "/static/js/main.1cf9b807.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
