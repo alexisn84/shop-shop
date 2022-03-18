7498dec99c19dc28f4bd6f30b883e4d3572987c8
alexisn84 <elamrani.alexis@gmail.com>
2022-03-18 09:45:41 -0400

    stuff

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -1,9 +1,9 @@
 import React, { useEffect } from 'react';
 import { useQuery } from '@apollo/client';
-import { useStoreContext } from '../..//utils/GlobalState';
-import { UPDATE_PRODUCTS } from '../../utils/actions';
 
 import ProductItem from '../ProductItem';
+import { useStoreContext } from '../../utils/GlobalState';
+import { UPDATE_PRODUCTS } from '../../utils/actions';
 import { QUERY_PRODUCTS } from '../../utils/queries';
 import spinner from '../../assets/spinner.gif';
 
@@ -18,7 +18,7 @@ function ProductList() {
     if (data) {
       dispatch({
         type: UPDATE_PRODUCTS,
-        products: data.products
+        products: data.products,
       });
     }
   }, [data, dispatch]);
@@ -28,7 +28,9 @@ function ProductList() {
       return state.products;
     }
 
-    return state.products.filter(product => product.category._id === currentCategory);
+    return state.products.filter(
+      (product) => product.category._id === currentCategory
+    );
   }
 
   return (
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,16 +1,17 @@
 import React from "react";
 import ProductList from "../components/ProductList";
 import CategoryMenu from "../components/CategoryMenu";
+//import Cart from "../components/Cart";
 
 const Home = () => {
-  //const [currentCategory, setCategory] = useState("");
-
   return (
     <div className="container">
-      <CategoryMenu  />
+      <CategoryMenu />
       <ProductList />
+      {/* <Cart /> */}
     </div>
   );
 };
 
 export default Home;
+
